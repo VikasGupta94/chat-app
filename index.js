47a678346044cabb4b43e8d2f5e3403b071c217a
Vikas <vikas1766gupta@gmail.com>
2021-09-29 04:56:52 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express= require("express");
+const app=express();
+const http = require("http").Server(app);
+const io = require('socket.io')(http);
+const path=require('path');
+const users = {};
+
+const staticpath=path.join(__dirname,"/public");
+app.use(express.static(staticpath));
+io.on("connection", (socket) => {
+    socket.on('new-user-join', (name) => {
+        console.log(name);
+        users[socket.id] = name;
+
+        socket.broadcast.emit('user-joind', name);
+    });
+socket.on("send",(message)=>{
+    
+    socket.broadcast.emit("recieve",{message:message, name:users[socket.id]})
+});
+});
+
+http.listen(3000, () => { console.log("listening") });
